3f9863dd687a49f52f228d73a8b6f950d20c4d15
Archana Prasad Nair <128218198+Archana-P-Nair@users.noreply.github.com>
2025-10-01 17:30:46 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // experimental: {
-  //   appDir: true,
-  // },
-}
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        canvas: false,
+      };
+    }
+    return config;
+  },
+};
 
-
-module.exports = nextConfig
+module.exports = nextConfig;
